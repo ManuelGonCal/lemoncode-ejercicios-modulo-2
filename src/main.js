2ef0ea674fa569ff1861f95b143920061e104642
Manuel Gonzalez <manuelmaingonzalez@gus.global>
2020-11-26 22:07:11 +0100

    modified index.html and main.js for new exercises logic

--- src/main.js
+++ src/main.js
@@ -1,12 +1,32 @@
 import ejercicio1 from "./ejercicio1/ejercicio1";
 import ejercicio2 from "./ejercicio2/ejercicio2";
+import ejercicio3 from "./ejercicio3/ejercicio3";
+import ejercicio4 from "./ejercicio4/ejercicio4";
+import ejercicio5 from "./ejercicio5/ejercicio5";
 
 function executeEjercicio() {
+    console.clear();
     const index = this.dataset.index;
     
     switch (index) {
-        case "1": ejercicio1();
-        case "2": ejercicio2();
+        case "1": 
+            ejercicio1();
+            break;
+        case "2": 
+            ejercicio2();
+            break;
+        case "3-a":
+            ejercicio3(true);
+            break;
+        case "3-b":
+            ejercicio3(false);
+            break;
+        case "4":
+            ejercicio4();
+            break;
+        case "5":
+            ejercicio5();
+            break;
     }
         
 }
